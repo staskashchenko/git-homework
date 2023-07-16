bdca1756d5bcaa4417999beda6802f9523868a47
muhamor92 <timhydeaustralia@gmail.com>
2023-07-16 12:50:42 +1000

    added a simple explanation as to what the programme does

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# 3 FEATURES:
+# 4 FEATURES:
 # license plate generator
 # pin number generator
 # valid email password generator
+# user interface
 
 def license_plate_generator():
     pass
@@ -11,3 +12,11 @@ def pin_number_generator():
 
 def valid_email_generator():
     pass
+
+print("CODE GENERATOR 1.0:")
+print()
+print("1. CAR LICENSE PLATE")
+print("2. PIN NUMBER")
+print("3. VALID EMAIL PASSWORD")
+print("4. EXIT")
+print()

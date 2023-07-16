7607690b97acdbe05e3ee628faa6a3ceb78ba719
Muhamor92 <timhydeaustralia@gmail.com>
2023-07-16 11:10:28 +1000

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+# 3 FEATURES:
+# license plate generator
+# pin number generator
+# valid email password generator
+
+def license_plate_generator():
+    pass
+
+def pin_number_generator():
+    pass
+
+def valid_email_generator():
+    pass

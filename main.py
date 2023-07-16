234a0fd717c57bf608563a944c2f031683a2638f
staskashchenko <stasspace12@gmail.com>
2023-07-16 13:23:38 +1000

    filled out the valid email password generator function
    most passwords these days must consist of at least 10 characters, with a minimum of 1 lowercase, 1 uppercase, 1 number and 1 special character.

--- main.py
+++ main.py
@@ -16,4 +16,23 @@ def pin_number_generator():
     return "".join(pin)
 
 def valid_email_generator():
-    pass
+    import string
+    import random
+    all_digits = string.digits
+    all_lowercase = string.ascii_lowercase
+    all_uppercase = string.ascii_uppercase
+    special_characters = ['@',"#","$","*","&","!"]
+    password = []
+
+    for letter in range(4):
+        password.append(random.choice(all_lowercase))
+
+    for digit in range(4):
+        password.append(random.choice(all_digits))
+
+    for capital in range(2):
+        password.append(random.choice(all_uppercase))
+
+    for special_char in range(2):
+        password.append(random.choice(special_characters))
+    return "".join(password)

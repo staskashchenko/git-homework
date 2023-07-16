a16c602186d3b01ccba68989bec1558734c3fa0c
muhamor92 <timhydeaustralia@gmail.com>
2023-07-16 12:53:39 +1000

    added a while loop for the user interface

--- main.py
+++ main.py
@@ -20,3 +20,21 @@ print("2. PIN NUMBER")
 print("3. VALID EMAIL PASSWORD")
 print("4. EXIT")
 print()
+
+run = True
+
+while run:
+    selected_code = input("What type of code would you like to generate (1,2 or 3) :")
+    if selected_code == '1':
+        print(license_plate_generator())
+        print()
+    elif selected_code == '2':
+        pin_number_generator()
+        print()
+    elif selected_code == '3':
+        valid_email_generator()
+        print()
+    elif selected_code == '4':
+        run = False
+    else:
+        print('Invalid input. Please enter a number from 1-4.')
